2cbb5af2db9a13115bed40b090a7aaa8afa9d560
Thunderw1nd <acodingape@hotmail.com>
2022-02-09 20:06:05 +0800

    Uploaded About.vue and a temporary image source

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,10 +7,10 @@
     </div>
     <div id="BodyPart">
           <div id="LeftPart">
-            <div id="Text">
-              <p>测试文本测试文本</p>
-              <br>
-              <p>测试文本测试文本</p>
+            <div id="Text" >
+              <p class="UnderLine">测试文本测试文本</p>
+              <p class="UnderLine">测试文本测试文本</p>
+              <p class="UnderLine">测试文本测试文本</p>
               <br>
             </div>
             <div id="Slogan">
@@ -21,7 +21,18 @@
             </div>
           </div>
           <div id="RightPart">
-            <img src="../assets/cherry.png">
+            <div id="TopPart">
+              <div class="Section" id="TopLeft">
+                <p class="SectionText">测试文本1</p>
+              </div>
+              <div class="Section" id="TopRight">
+                <p class="SectionText">测试文本2</p>
+              </div>
+            </div>
+            <div id="BottomPart">
+              <div class="Section" id="BottomLeft"><p class="SectionText">测试文本3</p></div>
+              <div class="Section" id="BottomRight"><p class="SectionText">测试文本4</p></div>
+            </div>
           </div>
     </div>
 </template>
@@ -39,7 +50,7 @@
 </script>
 
 <style scoped>
-    #Background{
+  #Background{
     background: url("../assets/main.jpg") center no-repeat;
     background-size: cover;
   }
@@ -56,11 +67,11 @@
     color: #2464a4;
     opacity: 1!important;
   }
-  #Text{
-    border-style: double;
-    padding: 30px;
-    border-width: 20px;
-    border-radius: 15px;
+  .UnderLine{
+    border-bottom-style: solid;
+    border-bottom-color: gray;
+    border-bottom-width: 2px;
+    font-size: 30px;
   }
   #BodyPart{
     
@@ -74,19 +85,66 @@
     font-size: 36px;
   }
   #LeftPart{
-    padding-right: 100px;
+    
     float: left;
   }
   #RightPart{
-    padding-left: 100px;
-    padding-bottom: 300px;
+    margin-left: 50px;
+    background: url("../assets/ideasvg.svg") center no-repeat;
+    width: 500px;
+    height: 500px;
     float: right;
   }
   #Slogan{
+    border-top-style: solid;
+    border-top-color: #b63f34;
+    border-top-width: 6px;
     padding-top: 10px;
     font-weight: bold;
     font-size: 45px;
     margin: 10px;
+  }
+  #Idea{
+    transform: scale(1.3);
+  }
+  #TopPart{
+    height: 250px;
+    width: 500px;
+  }
+  #BottomPart{
+    height: 250px;
+    width: 500px;
+  }
+  .Section{
+    height: 100%;
+    width: 50%;
+    padding:0px;
+    margin: 0px;
+  }
+  .Section:hover{
+    background-color: white;
+    box-shadow: 10px 10px 8px #888888;
+  }
+  .Section:hover .SectionText{
+    display:block;
+    text-align:center;
+  }
+  .SectionText{
+    align-content: center;
+    text-align: center;
+    display: none;
+  }
+  #TopLeft{
+    float: left;
+  }
+  #TopRight{
+    float: right;
+  }
+  #BottomLeft{
+    float:left;
+  }
+  #BottomRight{
+    float: right;
   }
 </style>
 
