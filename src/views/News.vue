09b67f38424db3e06ed63b6c16f515647b43b152
Henry3510 <30743133+Henry3510@users.noreply.github.com>
2022-02-16 18:04:43 +0800

    move News.vue to the correct place

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,18 +1,54 @@
-<template>
-  <h1>NEWS</h1>
-</template>
-
-<script>
-import FooterVue from "../components/Footer.vue";
-import HeaderVue from "../components/Header.vue";
-
-export default {
-  components: {
-    HeaderVue,
-    FooterVue,
-  },
-};
+<script setup>
+import NewsBlockLeft from '../components/NewsBlockLeft.vue'
+import NewsBlockRight from '../components/NewsBlockRight.vue'
+import NewsImages from "../assets/Main.jpg"
 </script>
 
+<template>
+  <img :src='NewsImages' alt='' class='background'>
+  <div class="main">
+    <h1 class="title">N<span style="color: var(--baid-red);">E</span>WS</h1> 
+    <NewsBlockRight content="在此输入文字" title="在此输入文字" :imageurl='NewsImages' subtitle="年份/月份/日期" link="还没做跳转功能"></NewsBlockRight>
+    <NewsBlockLeft content="在此输入文字" title="在此输入文字" :imageurl='NewsImages' subtitle="年份/月份/日期" link="还没做跳转功能"></NewsBlockLeft>
+    <NewsBlockRight content="在此输入文字" title="在此输入文字" :imageurl='NewsImages' subtitle="年份/月份/日期" link="还没做跳转功能"></NewsBlockRight>
+    <NewsBlockLeft content="在此输入文字" title="在此输入文字" :imageurl='NewsImages' subtitle="年份/月份/日期" link="还没做跳转功能"></NewsBlockLeft>
+    <NewsBlockRight content="在此输入文字" title="在此输入文字" :imageurl='NewsImages' subtitle="年份/月份/日期" link="还没做跳转功能"></NewsBlockRight>
+    <NewsBlockLeft content="在此输入文字" title="在此输入文字" :imageurl='NewsImages' subtitle="年份/月份/日期" link="还没做跳转功能"></NewsBlockLeft>
+  </div>
+</template>
+
 <style scoped>
+.background {
+  content:fixed;
+  height:100vh;
+  width:100vw;
+  object-fit: cover;
+  position:fixed;
+  left:0px;
+  top:0px;
+  z-index:-3;
+  filter:brightness(60%);
+}
+.main {
+  width:65vw;
+  max-width:1000px;
+  background:white;
+  position:relative;
+  margin:auto;
+  padding:20px;
+}
+.title {
+  text-align:middle;
+  color: var(--baid-blue);
+  margin-left: 15px;
+  font-size: 65px;
+}
+@media screen and (max-width: 800px) {
+  .title {
+    margin-left: 20px;
+  }
+   .blocker{
+    display:none;
+  }
+}
 </style>
